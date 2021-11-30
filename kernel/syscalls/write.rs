6a619edc5382c1b3488e64051c183ab0072684b0
zooltd <youhan.wu@outlook.com>
2021-11-30 03:32:26 -0600

    echo: write.rs test

--- kernel/syscalls/write.rs
+++ kernel/syscalls/write.rs
@@ -3,11 +3,19 @@ use crate::prelude::*;
 use crate::{fs::opened_file::Fd, user_buffer::UserBuffer};
 use crate::{process::current_process, syscalls::SyscallHandler};
 use core::cmp::min;
-use kerla_runtime::address::UserVAddr;
+use kerla_runtime::{address::UserVAddr, print::get_printer};
 
 impl<'a> SyscallHandler<'a> {
     pub fn sys_write(&mut self, fd: Fd, uaddr: UserVAddr, len: usize) -> Result<isize> {
         let len = min(len, MAX_READ_WRITE_LEN);
+
+        let mut tmp = vec![0; len];
+        let copied_len = uaddr.read_cstr(tmp.as_mut_slice())?;
+        let content = core::str::from_utf8(&tmp[..copied_len])
+        .map_err(|_| Error::new(Errno::EINVAL))?
+        .to_owned();
+
+        get_printer().print_str(&content);
 
         let opened_file = current_process().get_opened_file_by_fd(fd)?;
         trace!(
