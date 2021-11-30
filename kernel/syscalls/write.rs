c9724b92cf7560ef245f683e5acc6e1e67be5af4
zooltd <youhan.wu@outlook.com>
2021-11-30 16:06:59 -0600

    echo: write.rs √

--- kernel/syscalls/write.rs
+++ kernel/syscalls/write.rs
@@ -1,23 +1,18 @@
 use super::MAX_READ_WRITE_LEN;
 use crate::prelude::*;
+use crate::user_buffer::UserCStr;
 use crate::{fs::opened_file::Fd, user_buffer::UserBuffer};
 use crate::{process::current_process, syscalls::SyscallHandler};
 use core::cmp::min;
+use core::mem::size_of;
 use kerla_runtime::{address::UserVAddr, print::get_printer};
 
 impl<'a> SyscallHandler<'a> {
     pub fn sys_write(&mut self, fd: Fd, uaddr: UserVAddr, len: usize) -> Result<isize> {
         let len = min(len, MAX_READ_WRITE_LEN);
 
-        let mut tmp = vec![0; len];
-        let copied_len = uaddr.read_cstr(tmp.as_mut_slice())?;
-        let content = core::str::from_utf8(&tmp[..copied_len])
-        .map_err(|_| Error::new(Errno::EINVAL))?
-        .to_owned();
-
-        get_printer().print_str(&content);
-
         let opened_file = current_process().get_opened_file_by_fd(fd)?;
+
         trace!(
             "[{}:{}] write(file={:?}, len={})",
             current_process().pid().as_i32(),
@@ -26,7 +21,27 @@ impl<'a> SyscallHandler<'a> {
             len
         );
 
+        /* ===================================================================== */
         let written_len = opened_file.write(UserBuffer::from_uaddr(uaddr, len))?;
+
+        let content = UserCStr::new(uaddr, len)?.as_str().to_owned();
+
+        let mut split = content.split('_');
+
+        let instruction: (String, String, String) = (
+            split.next().unwrap().trim().to_string(),
+            split.next().unwrap().trim().to_string(),
+            split.next().unwrap().trim().to_string(),
+        );
+
+        if instruction.0 == "add" {
+            let num1 = instruction.1.to_owned().parse::<i32>().unwrap();
+            let num2 = instruction.2.to_owned().parse::<i32>().unwrap();
+            let ans = num1 + num2;
+            let output = format!("{} + {} = {}\n", num1, num2, ans);
+            get_printer().print_str(&output);
+        }
+        /* ===================================================================== */
 
         // MAX_READ_WRITE_LEN limit guarantees total_len is in the range of isize.
         Ok(written_len as isize)
